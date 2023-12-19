4d61a02ce78ec9f546953da4e2f04e87603c5db9
donarid <David.Donari-non-empl@moodys.com>
2023-12-19 14:32:16 -0300

    Add project files.

---
+++ GiveFreelyChallenge_Domain/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using GiveFreelyChallenge_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiveFreelyChallenge_Domain.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.Entity<ApplicationUserChatroom>()
+            //.HasKey(bc => new { bc.ApplicationUserId, bc.ChatroomId });
+            //builder.Entity<ApplicationUserChatroom>()
+            //    .HasOne(bc => bc.ApplicationUser)
+            //    .WithMany(b => b.Chatrooms)
+            //    .HasForeignKey(bc => bc.ApplicationUserId);
+            //builder.Entity<ApplicationUserChatroom>()
+            //    .HasOne(bc => bc.Chatroom)
+            //    .WithMany(c => c.Members)
+            //    .HasForeignKey(bc => bc.ChatroomId);
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Affiliate> Affiliates { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ GiveFreelyChallenge_Domain/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace GiveFreelyChallenge_Domain.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..", "GiveFreelyChallenge"))
+                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                 .Build();
+
+            var connectionString = configuration.GetSection("ConnectionStrings:DefaultConnection").Value;
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseLoggerFactory(GetLoggerFactory()).UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+
+        private ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+                   builder.AddFilter(DbLoggerCategory.Database.Command.Name,
+                                     LogLevel.Information));
+            return serviceCollection.BuildServiceProvider()
+                    .GetService<ILoggerFactory>();
+        }
+    }
+}
