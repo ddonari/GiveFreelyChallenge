0a771d4369581798c5bd8c842e87acf66db1f4cd
donarid <David.Donari-non-empl@moodys.com>
2023-12-19 17:19:45 -0300

    Add Affiliates & Customer Controller

---
+++ GiveFreelyChallenge/Controllers/CustomersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveFreelyChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        // Implement CRUD operations for customers
+    }
+
+}
