a5aa63ce3cd4d77f3bfc50e52f77b376488c49c4
donarid <David.Donari-non-empl@moodys.com>
2023-12-21 15:47:03 -0300

    Removed initial controllers and Add Customer class

--- GiveFreelyChallenge_Domain/Models/Customer.cs
+++ GiveFreelyChallenge_Domain/Models/Customer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GiveFreelyChallenge.Domain.Models
+﻿namespace GiveFreelyChallenge.Domain.Models
 {
     public class Customer
     {
